5faa336b0570830d7c156d4f54689f07b0a44d86
Natan moreira <natan-moreira1@hotmail.com>
2024-02-11 15:43:44 -0300

    chore: install react-query and config redirect on next js

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
   images: {
     domains: ['openweathermap.org'],
   },
+  async redirects() {
+    return[
+      {
+      source: '/',
+      destination: '/dashboard',
+      permanent: true
+    }]
+  },
 };
 
 export default nextConfig;
